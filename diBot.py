07be58a9710bcce36fa2fd2e24700edf21cdd93d
AlenisLennelluc <37004941+AlenisLennelluc@users.noreply.github.com>
2018-07-27 18:28:55 -0700

    Moving and Adding
    Changed the base file around to make it a bit more organized, and added a second file that currently refuses to function.

--- diBot.py
+++ diBot.py
@@ -5,6 +5,8 @@ import logging
 import time
 import random
 
+import Forest_hut
+
     
 
 
@@ -25,10 +27,11 @@ class player():
     def __init__(self):
         self.Location = area()
         self.Username = ''
+        self.secret = False
+        self.greet_function = greeter
 
 client = discord.Client()
 PlayerList = {}
-
 
 forest = area()
 forest.areaName ='The great forest'
@@ -51,8 +54,10 @@ async def on_ready():
 async def on_message(message):
     global PlayerList
 
+    if (message.author.id in PlayerList):
+        await PlayerList[message.author.id].greet_function(message)
     
-    if message.content.startswith('!test'):
+    elif message.content.startswith('!test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -65,24 +70,10 @@ async def on_message(message):
         await client.send_message(message.channel, 'Done sleeping')
 
     elif message.content.lower().startswith('!checkin'):
-        if (message.author.id in PlayerList):
-            print(message.author.id)
-            print(PlayerList.keys())
-            await client.send_message(message.channel, 'Hello %s, Welcome back.' % PlayerList[message.author.id].Username)
-            await client.send_message(message.channel, 'You are in the {}. Would you like to look around?'.format(PlayerList[message.author.id].Location.areaName))
-
-            def check(msg):
-                return msg.content.startswith('$look around')
-            
-            message = await client.wait_for_message(author=message.author, check=check)
-            #if ()
-            await client.send_message(message.channel, PlayerList[message.author.id].Location.areaDescription)
-            
-        else:
-            await client.send_message(message.channel,
-                                      'Hello %s Welcome to Discordia! This is an interactive text based MMO \nwhich is currently under production'
-                                      % message.author.name )
-            await client.send_message(message.channel, 'If you would like to create a profile please type \"!d create profile\" ')
+        await client.send_message(message.channel,'''
+Hello %s, Welcome to Discordia!
+This is an interactive text based MMO which is currently under production.''' % message.author.name )
+        await client.send_message(message.channel, 'If you would like to create a profile please type \"!d create profile\" ')
 
     elif message.content.startswith('!d create profile'):
         await client.send_message(message.channel, 'Thank you for joining this project')
@@ -109,7 +100,34 @@ async def on_message(message):
         file2 = open('PlayerData', 'rb')
         PlayerList = pickle.load(file2)
         file2.close()
+
+    
+
+async def greeter(message):
+    if message.content.lower().startswith('!checkin'):
+        print(message.author.id)
+        print(PlayerList.keys())
+        await client.send_message(message.channel, 'Hello %s, Welcome back.' % PlayerList[message.author.id].Username)
         
+        await client.send_message(message.channel, 'You are in the {}. Would you like to look around?'.format(PlayerList[message.author.id].Location.areaName))
+
+        def check(msg):
+            return msg.content.startswith('$look around')
+            
+        message = await client.wait_for_message(author=message.author, check=check)
+        #if ()
+        await client.send_message(message.channel, PlayerList[message.author.id].Location.areaDescription)
+
+    elif message.content.startswith('$walk through forest'):
+        await client.send_message(message.channel, 'You walk through the forest, passing by the various life in the forest.')
+
+        def check(msg):
+            return msg.content.startswith('$walk through the dark side')
+
+        message = await client.wait_for_message(author = message.author, check = check)
+        PlayerList[message.author.id].secret = True
+        Forest_hut.player_enter(message)
+        PlayerList[message.author.id].greet_function = Forest_hut.greeter()
         
 client.run('Mjg4OTY2NjI1MjAwMTc3MTUy.DjZkJA.aRmsmA2Yq7AjH5e5VIoqMiG_ftQ')
 
